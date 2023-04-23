0f12d52f40a92ea76b7628bac0476dee4b04b663
unknown <you@example.com>
2023-04-23 16:31:35 -0300

    fetch

--- 2_promise_rejected.js
+++ 2_promise_rejected.js
@@ -1,7 +1,7 @@
 const myPromise = new Promise((resolve, reject) =>
-	reject("myPromise error")
+	reject("error")
 )
 
 myPromise
-	//.then(result => console.log(result))
+	.then(result => console.log(result)) // resolve
 	.catch(error => console.log(error))
